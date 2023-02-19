6cdb25b6f30414e76442baf60f61e7ac9d3c2608
Gong Xilong （宫喜龙） <122077814+GongXilong1@users.noreply.github.com>
2023-02-19 21:54:12 +0800

    Add files via upload

---
+++ Project_Exercise/12_sql/SQL_Script/Script-4.sql
@@ -0,0 +1,10 @@
+--  DQL基础查询
+
+select id, name, age from newstu;  # 从newstu这个表中选择id和name这两个列进行展示.
+
+select * from newstu;   -- 查询newstu表中全部的列.
+
+select * from newstu where age > 20;   --  查询newstu表中age大于20的数据
+
+select * from newstu where gender = '男';   --  查询newstu表中性别为男的数据
+
